5874070299f6e16c8f23346c547d033ca9e1825f
Michael Strange <mstrange@kloveair1.com>
2021-12-27 09:07:25 -0800

    All inputs

--- Project/Models/Inputs.cs
+++ Project/Models/Inputs.cs
@@ -8,12 +8,27 @@ using System.Threading.Tasks;
 namespace SantaPuppet.Models.Inputs
 {
     public class Inputs
-    {
+    {        
+        /// <summary>
+        /// GPIO-4. Left Arm Stops(Red)
+        /// </summary>
+        public static int SantaLeftArmStop { get; } = 4;
+     
+        /// <summary>
+        /// GPIO-10. Right Arm Stops(Green)
+        /// </summary>
+        public static int SantaRightArmStop { get; } = 10;
 
-        //GPIO-4. Left Arm Stops(Red)
-        //GPIO-10. Right Arm Stops(Green)
-        //GPIO-11. Feet Stops(Blue)
-        //GPIO-17. Twist Stops(Black)
+        /// <summary>
+        /// GPIO-11. Feet Stops(Blue)
+        /// </summary>
+        public static int SantaFeetStop { get; } = 11;
+
+        /// <summary>
+        /// GPIO-17. Twist Stops(Black)
+        /// </summary>
+        public static int SantaTwistStop { get; } = 17;
+
       
         /// <summary>
         /// GPIO-8. Curtin Stage Left Stop(Open) (Yellow)
@@ -27,7 +42,7 @@ namespace SantaPuppet.Models.Inputs
 
  
         /// <summary>
-        /// GPIO-26. Play button - Input
+        /// GPIO-26. Play button (Blue)
         /// </summary>
         public static int PlayButton { get; } = 26;
 
@@ -35,16 +50,15 @@ namespace SantaPuppet.Models.Inputs
         /// Open all the input pins
         /// </summary>
         /// <param name="controller"></param>
-        public static void OpenPins(GpioController controller) {
-         
+        public static void OpenPins(GpioController controller) 
+        {
+            controller.OpenPin(SantaLeftArmStop, PinMode.Input);
+            controller.OpenPin(SantaRightArmStop, PinMode.Input);
+            controller.OpenPin(SantaFeetStop, PinMode.Input);
+            controller.OpenPin(SantaTwistStop, PinMode.Input);
             controller.OpenPin(PlayButton, PinMode.Input); 
             controller.OpenPin(CurtinStageLeftStopOpen, PinMode.Input);
             controller.OpenPin(CurtinStageRightStopClosed, PinMode.Input);
-        }
-
-   
-
-
-
+        } 
     }
 }
