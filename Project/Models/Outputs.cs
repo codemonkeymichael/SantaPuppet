020f8733d30bf8297ebb86f89996ce8bd1e86337
Michael Strange <mstrange@kloveair1.com>
2022-01-23 22:51:25 -0800

    New Out Pins

--- Project/Models/Outputs.cs
+++ Project/Models/Outputs.cs
@@ -3,67 +3,76 @@
 public class Lights
 {
 
-    /// <summary>
-    /// GPIO-27. Play Btn Feedback LED Green
-    /// </summary>
-    public static int PlayBtnGreen { get; } = 27;
+    /// MCP23017 Port Expander 0x20 
+    /// https://raspi.tv/wp-content/uploads/2013/07/MCP23017.jpg 
+
 
     /// <summary>
-    /// GPIO-15. Play Btn Feedback LED Red
+    /// PI-GPIO-14. Play Btn Feedback LED Green
     /// </summary>
-    public static int PlayBtnRed { get; } = 15;
+    public static int PlayBtnGreen { get; } = 14;
+
 
     /// <summary>
-    /// GPIO-18. Key Lights(PWM Dimmable Channel 0)
+    /// PI-GPIO-18. Key Lights(PWM Dimmable Channel 0)
     /// </summary>
     private static int KeyLights { get; } = 18;
 
+
+
     /// <summary>
-    /// GPIO-19. Foot Lights(PWM Dimmable Channel 1)
+    /// GPIO-B7-16. Play Btn Feedback LED Red
+    /// </summary>
+    public static int PlayBtnRed { get; } = 15;
+
+
+
+    /// <summary>
+    /// PI-GPIO-19. Foot Lights(PWM Dimmable Channel 1)
     /// </summary>
     private static int FootLights { get; } = 19;
 
 
     /// <summary>
     /// Backlight Truss
-    /// GPIO-5. Back Left Red(QA Pass)
+    /// MCP-GPIO-A0-1. Back Left Red (red wire)
     /// </summary>
-    public static int BackStageRightRed { get; } = 5;
+    public static int BackStageRightRed { get; } = 1;
 
     /// <summary>
-    /// GPIO-6. Back Left Green(QA Pass)
+    /// MCP-GPIO-A1-2. Back Left Green (green wire)
     /// </summary>
-    public static int BackStageRightGreen { get; } = 6;
+    public static int BackStageRightGreen { get; } = 2;
 
     /// <summary>
-    /// GPIO-7. Back Left Blue(QA Pass)
+    /// MCP-GPIO-A2. Back Left Blue (blue wire)
     /// </summary>
-    public static int BackStageRightBlue { get; } = 7;
+    public static int BackStageRightBlue { get; } = 3;
 
     /// <summary>
-    /// GPIO-12. Back Left Yellow(QA Pass)
+    /// MCP-GPIO-A3. Back Left Yellow (yellow Pass)
     /// </summary> 
-    public static int BackStageRightYellow { get; } = 12;
+    public static int BackStageRightYellow { get; } = 4;
 
     /// <summary>
-    /// GPIO-13. Back Right Red(QA Pass)
+    /// MCP-GPIO-A4. Back Right Red (red wire)
     /// </summary>
-    public static int BackStageLeftRed { get; } = 13;
+    public static int BackStageLeftRed { get; } = 5;
 
     /// <summary>
-    /// GPIO-16. Back Right Green(QA Pass)
+    /// MCP-GPIO-A5. Back Right Green (green wire)
     /// </summary>
-    public static int BackStageLeftGreen { get; } = 16;
+    public static int BackStageLeftGreen { get; } = 6;
 
     /// <summary>
-    /// GPIO-20. Back Right Blue(QA Pass)
+    /// MCP-GPIO-A6. Back Right Blue (blue wire)
     /// </summary>
-    public static int BackStageLeftBlue { get; } = 20;
+    public static int BackStageLeftBlue { get; } = 7;
 
     /// <summary>
-    /// GPIO-21. Back Right Yellow(QA Pass)
+    /// MCP-GPIO-A7. Back Right Yellow(yellow wire)
     /// </summary>
-    public static int BackStageLeftYellow { get; } = 21;
+    public static int BackStageLeftYellow { get; } = 8;
 
     public static int[] Backlights { get; } = new int[8]
     {
@@ -77,15 +86,17 @@ public class Lights
             BackStageLeftYellow
     };
 
-    public static void OpenPins(GpioController controller)
+    public static void OpenPins(GpioController piGPIOController, GpioController mcp20GPIOController)
     {
         //Button Lights
-        controller.OpenPin(PlayBtnRed, PinMode.Output, PinValue.Low);
-        controller.OpenPin(PlayBtnGreen, PinMode.Output, PinValue.Low);
+        piGPIOController.OpenPin(PlayBtnRed, PinMode.Output, PinValue.Low);
+
+        piGPIOController.OpenPin(PlayBtnGreen, PinMode.Output, PinValue.Low);
+
         //Stage Back Lights
         foreach (var pin in Backlights)
         {
-            controller.OpenPin(pin, PinMode.Output, PinValue.Low);
+            mcp20GPIOController.OpenPin(pin, PinMode.Output, PinValue.Low);
         }
 
     }
@@ -94,69 +105,68 @@ public class Lights
 public class Motors
 {
     /// <summary>
-    /// GPIO-14. Talking (Black)
+    /// PI-GPIO-15. Talking (Black) 
     /// </summary>
-    public static int SantaTalk { get; } = 14;
+    public static int SantaTalk { get; } = 15;
 
 
     /// <summary>
-    /// Curtin Motor GPIO 22, 23, 24, 25S
-    /// GPIO-22. Motor Curtin 1 (Red)
-    /// GPIO-23. Motor Curtin 2 (Green)
-    /// GPIO-24. Motor Curtin 3 (Blue)
-    /// GPIO-25. Motor Curtin 4 (Yellow)
+    /// Curtin Motor PI GPIO 21, 26, 13, 6
+    /// PI-GPIO-21. Motor Curtin 1 (red wire)
+    /// PI-GPIO-26. Motor Curtin 2 (green wire)
+    /// PI-GPIO-13. Motor Curtin 3 (blue wire)
+    /// PI-GPIO-6.  Motor Curtin 4 (yellow wire)   
     /// </summary>
-    public static int[] curtinMotor { get; } = new int[4] { 22, 23, 24, 25 };
+    public static int[] curtinMotor { get; } = new int[4] { 21, 26, 13, 6 };
 
-    //MCP23017 Port Expander 0x20 
-    //https://raspi.tv/wp-content/uploads/2013/07/MCP23017.jpg  
+
+
+
+    /// <summary>
+    /// Shoulders Motor PI GPIO 7, 12, 16, 20
+    /// PI-GPIO-7. Motor Shoulders (red wire)
+    /// PI-GPIO-12. Motor Shoulders (green wire)
+    /// PI-GPIO-16. Motor Shoulders (blue wire)
+    /// PI-GPIO-20. Motor Shoulders (yellow wire) 
+    /// </summary>
+    public static int[] shouldersMotor { get; } = new int[4] { 7, 12, 16, 20 };
+
+    /// <summary>
+    /// Feet Motor PI GPIO 5, 11, 9, 10
+    /// PI-GPIO-5. Motor Feet (red wire)
+    /// PI-GPIO-11. Motor Feet (green wire)
+    /// PI-GPIO-9. Motor Feet (blue wire)
+    /// PI-GPIO-10. Motor Feet (yellow wire) 
+    /// </summary>
+    public static int[] feetMotor { get; } = new int[4] { 5, 11, 9, 10 };
+
+    /// <summary>
+    /// Lefyt Arm Motor PI GPIO  22, 27, 17, 4
+    /// PI-GPIO-22. Motor Left Arm (red wire)
+    /// PI-GPIO-27. Motor Left Arm (green wire)
+    /// PI-GPIO-17. Motor Left Arm (blue wire)
+    /// PI-GPIO-4. Motor Left Arm (yellow wire) 
+    /// </summary>
+    public static int[] leftArmMotor { get; } = new int[4] { 22, 27, 17, 4 };
 
 
     /// <summary>
     /// MCP23017 0x20
-    /// GPIO-A0.Motor1 Shoulders (Red)
-    /// GPIO-A1.Motor1 Shoulders (Green)
-    /// GPIO-A2.Motor1 Shoulders (Blue)
-    /// GPIO-A3.Motor1 Shoulders (Yellow)
+    /// PI-GPIO-23. Motor Right Arm (red wire)
+    /// PI-GPIO-24. Motor Right Arm (green wire)
+    /// PI-GPIO-25. Motor Right Arm (blue wire)
+    /// PI-GPIO-8. Motor Right Arm (yellow wire) 
     /// </summary>
-    public static int[] shouldersMotor { get; } = new int[4] { 0, 1, 2, 3 };
-
-    /// <summary>
-    /// MCP23017 0x20
-    /// GPIO-A4.Motor2 Feet (Red)
-    /// GPIO-A5.Motor2 Feet (Green)
-    /// GPIO-A6.Motor2 Feet (Blue)
-    /// GPIO-A7.Motor2 Feet (Yellow)
-    /// </summary>
-    public static int[] feetMotor { get; } = new int[4] { 4, 5, 6, 7 };
-
-    /// <summary>
-    /// MCP23017 0x20
-    /// GPIO-B0.Motor3 Left Arm (Red)
-    /// GPIO-B1.Motor3 Left Arm (Green)
-    /// GPIO-B2.Motor3 Left Arm (Blue)
-    /// GPIO-B3.Motor3 Left Arm (Yellow)
-    /// </summary>
-    public static int[] leftArmMotor { get; } = new int[4] { 8, 9, 10, 11 };
-
-
-    /// <summary>
-    /// MCP23017 0x20
-    /// GPIO-B4.Motor4 Right Arm (Red)
-    /// GPIO-B5.Motor4 Right Arm (Green)
-    /// GPIO-B6.Motor4 Right Arm (Blue)
-    /// GPIO-B7.Motor4 Right Arm (Yellow)
-    /// </summary>
-    public static int[] rightArmMotor { get; } = new int[4] { 12, 13, 14, 15 };
+    public static int[] rightArmMotor { get; } = new int[4] { 23, 24, 25, 8 };
 
 
 
 
-    public static void OpenPins(GpioController piGPIOController, GpioController mcp20GPIOController)
+    public static void OpenPins(GpioController piGPIOController)
     {
-        piGPIOController.OpenPin(SantaTalk, PinMode.Output);
+        piGPIOController.OpenPin(SantaTalk, PinMode.Output, PinValue.Low);
 
-        CurtinCues cc = new CurtinCues(piGPIOController);
+        //CurtinCues cc = new CurtinCues(piGPIOController);
         foreach (int motor in curtinMotor)
         {
             piGPIOController.OpenPin(motor, PinMode.Output, PinValue.Low);
@@ -166,24 +176,24 @@ public class Motors
         //AnimationCues ac = new AnimationCues(piGPIOController, mcp20GPIOController);
         foreach (int motor in shouldersMotor)
         {
-            mcp20GPIOController.OpenPin(motor, PinMode.Output, PinValue.Low);
+            piGPIOController.OpenPin(motor, PinMode.Output, PinValue.Low);
         }
         //ac.TwistCenter();
 
         foreach (int motor in feetMotor)
         {
-            mcp20GPIOController.OpenPin(motor, PinMode.Output, PinValue.Low);
+            piGPIOController.OpenPin(motor, PinMode.Output, PinValue.Low);
         }
         //TODO: Center the feet motor
 
         foreach (int motor in leftArmMotor)
         {
-            mcp20GPIOController.OpenPin(motor, PinMode.Output, PinValue.Low);
+            piGPIOController.OpenPin(motor, PinMode.Output, PinValue.Low);
         }
         //TODO: Center the left arm motor
         foreach (int motor in rightArmMotor)
         {
-            mcp20GPIOController.OpenPin(motor, PinMode.Output, PinValue.Low);
+            piGPIOController.OpenPin(motor, PinMode.Output, PinValue.Low);
         }
 
     }
